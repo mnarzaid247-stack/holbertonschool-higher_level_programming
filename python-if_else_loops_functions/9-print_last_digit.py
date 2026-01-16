06d53cccee8851db9c070e98b1aa17bfd8ecd1f2
manar <mnarzaid247@gmail.com>
2026-01-16 08:17:43 +0000

    doing the task

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        print(number % 10)
+        return(number % 10)
+    else:
+        number = number * -1
+        print(number % 10)
+        return(number % 10)
