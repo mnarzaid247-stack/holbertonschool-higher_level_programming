d0c56233bb2bc8138c756fc608e56a8826b04b6a
manar <mnarzaid247@gmail.com>
2026-01-16 13:22:09 +0000

    doing the task

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+number = 0
+for i in range(122, 97, -1):
+    if (122 - i) % 2 == 0:
+        number = i
+    else:
+        number = i - 32
+    print("{}".format(chr(number)), end="")
