bde325f4a423fa745c6a4fbe04ddfff1089242d3
manar <mnarzaid247@gmail.com>
2026-01-18 19:42:37 +0000

    update

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return new_list
     else:
-        newlist[idx] = element
+        new_list[idx] = element
         return new_list
