7dd8f98730437497e8ebea58deb30e7fc943ce2b
manar <mnarzaid247@gmail.com>
2026-01-19 10:17:53 +0000

    update

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -12,5 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             return (tuple_a[0] + tuple_b[0], tuple_b[1] + 0)
         elif len(tuple_b) == 1:
             return (tuple_a[0] + tuple_b[0], 0)
-         else:
+        else:
             return (0, 0)
