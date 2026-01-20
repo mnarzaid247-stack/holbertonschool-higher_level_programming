f560c0e51dc6f5c62fad75db43b2a0e27fe56252
manar <mnarzaid247@gmail.com>
2026-01-20 09:05:27 +0000

    doing task0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_mat =[]
+    for row in matrix:
+        new_row = list(map(lambda x: x * x, row))
+        new_mat.append(new_row)
+    return new_mat
