c7508bf01cd23e07463bb7fb774cb12e6e719c3c
manar <mnarzaid247@gmail.com>
2026-01-20 09:06:46 +0000

    fixing it

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_mat =[]
+    new_mat = []
     for row in matrix:
         new_row = list(map(lambda x: x * x, row))
         new_mat.append(new_row)
