5a48053d6c66f4cfb414eb49a83dc32a717a6d42
manar <mnarzaid247@gmail.com>
2026-01-21 12:53:28 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
     roman_set = {
             "I" : 1,
@@ -17,7 +15,7 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         curr = roman_set[roman_string[i]]
         if i + 1 < len(roman_string):
-            next_value = roman_set[roman_string[i + 1]
+            next_value = roman_set[roman_string[i + 1]]
             if curr < next_value:
                 total = total - curr
             else:
