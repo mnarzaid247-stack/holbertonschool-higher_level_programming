c0d3ae3ddd212869d96df9fe65857d0332c2dd01
manar <mnarzaid247@gmail.com>
2026-01-21 07:58:00 +0000

    fixing it

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     key = a_dictionary.keys()
     sort_key = sorted(key)
-    return sort_key
+    for key, value in a_dictionary.items():
+        print(key, ":", value)
