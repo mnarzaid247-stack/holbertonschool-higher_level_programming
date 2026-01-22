3b9fec0aaddc734eb4c8a53d51b620c751728710
mnar <mnarzaid247@gmail.com.com>
2026-01-22 07:31:35 +0000

    idk

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
-            i = i + 1
             number = number + 1
         except IndexError:
             break
