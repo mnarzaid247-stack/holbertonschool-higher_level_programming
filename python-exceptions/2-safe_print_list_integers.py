55ea454485c7d7a40af894e01e7060cea4bbe487
mnar <mnarzaid247@gmail.com.com>
2026-01-22 07:43:43 +0000

    idk

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             number = number + 1
-        except IndexError:
-            break
         except (TypeError, ValueError):
             pass
         i = i + 1
---
+++ python-exceptions/test.py
@@ -0,0 +1,5 @@
+from your_file_name import safe_print_list_integers
+
+print(safe_print_list_integers([True, 1, 2], 3))
+print(safe_print_list_integers([False, 1, 2], 3))
+print(safe_print_list_integers([True, False], 2))
