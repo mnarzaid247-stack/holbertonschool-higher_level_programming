fcd89d7061d0eca397d3d2fe46b3ec1f35249db6
mnar <mnarzaid247@gmail.com.com>
2026-01-22 06:53:46 +0000

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,13 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     number = 0
     i = 0
-    try:
-        while i < x:
+    while i < x:
+        try:
             print(my_list[i], end="")
             number = number + 1
             i = i + 1
-        print()
-        return number
-    except:
-        print()
-        return number
+        except:
+            break
+    print()
+    return number
