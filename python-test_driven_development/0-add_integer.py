c0b95d54d30f63e49b7e54d1bd462e0a4d88bd08
manar <mnarzaid@gmail.com>
2026-01-24 12:39:43 +0000

    update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Module that provides a function to add two integers."""
 def add_integer(a, b=98):
+    """Adds two integers after validating and converting inputs."""
     if type(a) != int and type(a) != float:
         raise TypeError ("a must be an integer")
     if type(b) != int and type(b) != float:
