6905a3af84b19c06e4a2bb67f308405743476303
manar <mnarzaid@gmail.com>
2026-01-24 13:08:57 +0000

    doing the task

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that provides a function to print a formatted full name."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints 'My name is <first_name> <last_name>' after type validation."""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
