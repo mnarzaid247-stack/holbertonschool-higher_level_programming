42378686b3ef3aff94db7e33f747bda413536405
manar <mnarzaid247@gmail.com>
2026-01-25 11:50:54 +0000

    doing task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""creating a class"""
+
+
+class Square:
+    """creating a class named Square"""
+    def __init__(self, size=0):
+        """creating size"""
+        self.size = size
+
+    def area(self):
+        """creating an area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """creating a getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """creating a setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """creating a printing function"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
