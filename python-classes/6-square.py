8d6171f284998f669242bf2fde6912a6e8cbf208
manar <mnarzaid247@gmail.com>
2026-01-25 12:05:32 +0000

    update

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""creating a class"""
+"""creating a class."""
 
 
 class Square:
-    """creating a class named Square"""
+    """creating a class named Square."""
+    
     def __init__(self, size=0, position=(0, 0)):
-        """creating size and position"""
+        """creating size and position."""
         self.size = size
         self.position = position
 
     def area(self):
-        """creating an area"""
+        """creating an area."""
         return self.__size * self.__size
 
     @property
     def size(self):
-        """creating a getter"""
+        """creating a getter for size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """creating a setter"""
+        """creating a setter fir size."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -30,12 +31,12 @@ class Square:
 
     @property
     def position(self):
-        """creating a getter"""
+        """creating a getter for position."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """creating a setter"""
+        """creating a setter for position."""
         if (not isinstance(value, tuple) or len(value) != 2 or
                 not isinstance(value[0], int) or not isinstance(value[1], int) or
                 value[0] < 0 or value[1] < 0):
@@ -43,7 +44,7 @@ class Square:
         self.__position = value
 
     def my_print(self):
-        """creating a printing function"""
+        """creating a printing function."""
         if self.__size == 0:
             print()
             return
