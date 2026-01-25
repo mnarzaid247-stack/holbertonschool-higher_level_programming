2265a4ab746d4dfbc07f072c45ad0c3d84d7b8ca
manar <mnarzaid247@gmail.com>
2026-01-25 08:28:40 +0000

    doing task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating a class."""
+
+
+class Square:
+    """creating a class."""
+    def __init__(self, size):
+        """making the value private."""
+        self.__size = size
