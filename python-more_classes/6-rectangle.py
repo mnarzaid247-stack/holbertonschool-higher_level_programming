3c2ffeb9586f8f10a0795fb828c746df90abdde1
manar <mnarzaid247@gmail.com>
2026-01-26 11:59:04 +0000

    update

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """creating a class named Rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
