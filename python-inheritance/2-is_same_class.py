367184825230c06ba3241777ad774dba4c85c35e
manar <mnarzaid247@gmail.com>
2026-01-27 09:43:00 +0000

    update

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """creating a function checs if they are the same class"""
-    if type(obj) == a_class:
-        return True
-    else:
-        return False
+    return type(obj) is a_class
