9357166596d3487ec12f83d836b4b676d94daa50
manar <mnarzaid247@gmail.com>
2026-01-27 09:57:50 +0000

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """writhing the method of the function"""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
