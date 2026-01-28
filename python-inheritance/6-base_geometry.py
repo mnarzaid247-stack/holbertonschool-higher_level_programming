c9621b41b1a810b1d3363af16dd32a02f1ccd83e
manar <mnarzaid247@gmail.com>
2026-01-28 08:03:20 +0000

    doing task 7

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating a module"""
+
+
+class BaseGeometry:
+    """creating a class called BaseGeometry"""
+    def area(self):
+        """creating a method"""
+        raise Exception("area() is not implemented")
