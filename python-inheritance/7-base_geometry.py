26c6fe30d155d8f90e9a174594731f550e24ed29
manar <mnarzaid247@gmail.com>
2026-01-28 08:12:27 +0000

    doing task8

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""creating a module"""
+
+
+class BaseGeometry:
+    """creating a class called BaseGeometry"""
+    def area(self):
+        """creating a method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """creating a function"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
