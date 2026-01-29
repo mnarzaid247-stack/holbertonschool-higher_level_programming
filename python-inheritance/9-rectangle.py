fc36e6763fbde3f15c70b6df0a13917bde133469
manar <mnarzaid247@gmail.com>
2026-01-29 07:19:15 +0000

    doing task8

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""creating a module"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry."""
+    def __init__(self, width, height):
+        """creating a function"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """creating an area method"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """creating a print way"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
