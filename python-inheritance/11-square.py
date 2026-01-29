04507ef78e083a15b9008ac727d5fc0af32a077a
manar <mnarzaid247@gmail.com>
2026-01-29 07:30:17 +0000

    doing task 10

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""creating a module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """creating a class"""
+    def __init__(self, size):
+        """creating a function"""
+        self.integer_validator("size", size)
+        super().__init(size, size)
+        self.__size = size
+
+    def area(self):
+        """creating an area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """creating a way to return a string"""
+        return f"[Square] {self.__size}/{self.__size}"
