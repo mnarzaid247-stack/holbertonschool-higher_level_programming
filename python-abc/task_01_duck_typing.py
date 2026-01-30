804b9755c06591d22573d463acfca186f92393eb
manar <mnarzaid247@gmail.com>
2026-01-30 15:02:59 +0000

    task1 again

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,15 +24,12 @@ class Circle(Shape):
     """Circle shape."""
 
     def __init__(self, radius):
-        """Initialize a circle with a radius."""
         self.radius = radius
 
     def area(self):
-        """Return the area of the circle."""
         return 3.141592653589793 * (self.radius ** 2)
 
     def perimeter(self):
-        """Return the perimeter of the circle."""
         return 2 * 3.141592653589793 * self.radius
 
 
@@ -40,16 +37,13 @@ class Rectangle(Shape):
     """Rectangle shape."""
 
     def __init__(self, width, height):
-        """Initialize a rectangle with width and height."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Return the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
@@ -57,9 +51,5 @@ def shape_info(shape):
     """
     Print the area and perimeter of a shape-like object.
     """
-    area = getattr(shape, "area")
-    perimeter = getattr(shape, "perimeter")
-    area = area() if callable(area) else area
-    perimeter = perimeter() if callable(perimeter) else perimeter
-    print(f"Area: {area}")
-    print(f"Perimeter: {perimeter}")
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
