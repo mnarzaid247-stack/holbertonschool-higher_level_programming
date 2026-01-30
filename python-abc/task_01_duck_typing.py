0b58d1a38173f5d19707d5d6dda7f929de3a9cf4
manar <mnarzaid247@gmail.com>
2026-01-30 12:02:12 +0000

    doing task1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+import math
+"""importing library"""
+
+
+class Shape(ABC):
+"""creating a class"""
+    @abstractmethod
+    def area(self):
+        """creating a area"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """creating a gitter"""
+        pass
+
+
+class Circle(Shape):
+"""creating a class"""
+    def __init__(self, radius):
+        """creating a method"""
+        self.radius = radius
+
+    def area(self):
+        """creating a area"""
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """creating a gitter"""
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+"""creating a class"""
+    def __init__(self, width, height):
+        """creating a mithod"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
