e156af674c71e3fcc8d09ca41e70b81d29d58715
manar <mnarzaid247@gmail.com>
2026-01-30 14:30:56 +0000

    update

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -62,6 +62,6 @@ def shape_info(shape):
         area = shape.area()
         perimeter = shape.perimeter()
     except AttributeError:
-        return
+        pass
     print(f"Area: {area}")
     print(f"Perimeter: {perimeter}")
