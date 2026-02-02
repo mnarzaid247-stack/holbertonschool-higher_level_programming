9c011d0526cf94fe7439e5c1ffb1eed77b6a1c67
manar <mnarzaid247@gmail.com>
2026-02-02 08:00:23 +0000

    doing task5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creating a module"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write an object to a file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
