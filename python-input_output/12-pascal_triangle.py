9a958be1487c40471f6197ab24adc37ae4bc0b60
manar <mnarzaid247@gmail.com>
2026-02-04 06:41:53 +0000

    update

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,10 +10,9 @@ def pascal_triangle(n):
     tri = [[1]]
     for i in range(1, n):
         prev = tri[-1]
-        new = [1]
+        new_row = [1]
         for j in range(len(prev) - 1):
             new_row.append(prev[j] + prev[j + 1])
         new_row.append(1)
         tri.append(new_row)
     return tri
-
