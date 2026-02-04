bbc1dc5cf9b3f45e6f2ade8d4da1356c362440c1
manar <mnarzaid247@gmail.com>
2026-02-04 06:40:18 +0000

    doing last task

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""creating a module"""
+
+
+def pascal_triangle(n):
+    """creating a defntion"""
+    new_list =[]
+    if n <= 0:
+        return new_list
+    tri = [[1]]
+    for i in range(1, n):
+        prev = tri[-1]
+        new = [1]
+        for j in range(len(prev) - 1):
+            new_row.append(prev[j] + prev[j + 1])
+        new_row.append(1)
+        tri.append(new_row)
+    return tri
+
