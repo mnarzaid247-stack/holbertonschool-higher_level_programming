c13a695538886258a9808042857319507d8d2f54
manar <mnarzaid247@gmail.com>
2026-02-06 13:38:52 +0000

    doing task0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a Python dictionary to JSON and save it to a file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def load_and_deserialize(filename):
+    """Load JSON from a file and deserialize it into a Python dictionary."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
