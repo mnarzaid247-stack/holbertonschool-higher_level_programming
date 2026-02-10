0badd3095ba5fe0b5fe9f2d6d4af74a19220a24b
manar <mnarzaid247@gmail.com>
2026-02-10 20:14:43 +0000

    update

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -14,7 +14,7 @@ def fetch_and_print_posts():
 def fetch_and_save_posts():
     req = requests.get("https://jsonplaceholder.typicode.com/posts")
     res = req.json()
-    with open("posts.csv", "w", newline="") as file:
+    with open("posts.csv", "W", newline="") as file:
         names= ["id", "title", "body"]
         my_file = csv.DictWriter(file, fieldnames=names)
         writer.writeheader()
