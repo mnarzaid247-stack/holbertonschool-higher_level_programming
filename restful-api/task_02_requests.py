b995dd94075cf3e56dafffdbc8870c44bf755470
manar <mnarzaid247@gmail.com>
2026-02-11 09:53:41 +0000

    update

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,21 +4,21 @@ import requests
 import csv
 
 def fetch_and_print_posts():
-    req = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if req.status_code == 200:
-        print(req.status_code)
-    res = req.json()
-    for i in res:
+    request = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if request.status_code == 200:
+        print("Status Code: ",request.status_code)
+    response = request.json()
+    for i in response:
         print(i["title"])
 
 def fetch_and_save_posts():
-    req = requests.get("https://jsonplaceholder.typicode.com/posts")
-    res = req.json()
+    request = requests.get("https://jsonplaceholder.typicode.com/posts")
+    response = request.json()
     with open("posts.csv", "W", newline="") as file:
-        names= ["id", "title", "body"]
+        names = ["id", "title", "body"]
         my_file = csv.DictWriter(file, fieldnames=names)
         writer.writeheader()
-        for post in res:
-            writer.writeheader({"id": post["id"],
-                "title": post["title"],
-                "body": post["body"]})
+        for i in response:
+            writer.writeheader({"id": i["id"],
+                "title": i["title"],
+                "body": i["body"]})
