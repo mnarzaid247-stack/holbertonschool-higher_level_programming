29bdbc4c8dd371d5ad32ed5c659a358980c1b39d
manar <mnarzaid247@gmail.com>
2026-02-13 14:36:39 +0000

    update

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -45,3 +45,49 @@ def jwt_invalid_token(err_msg):
 @jwt.expired_token_loader
 def jwt_expired_token(jwt_header, jwt_payload):
     return jsonify({"error": "Token has expired"}), 401
+
+@jwt.revoked_token_loader
+def jwt_revoked_token(jwt_header,jwt_payload):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def jwt_fresh_required(jwt_header, jwt_payload):
+    return jsonify({"error": "Fresh token required"}), 401
+
+@app.route("/basic-protected", methods=["GET"])
+@auth.login_required
+def basic_protected():
+    return "Basic Auth: Access Granted"
+
+@app.route("/login", methods=["POST"])
+def login():
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON"}), 401
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if not username or not password:
+        return jsonify({"error": "Invalid credentials"}), 401
+    if username not in users:
+        return jsonify({"error": "Invalid credentials"}), 401
+    if not check_password_hash(users[username]["password"], password):
+        return jsonify({"error": "Invalid credentials"}), 401
+    token = create_access_token(
+            identity=username,
+            additional_claims={"role": users[username]["role"]}
+            )
+    return jsoify({"access_token": token})
+
+@app.route("/jwt-protected", method=["get"])
+@jwt_required()
+def jwt_protected():
+    return "JWT Auth: Access Granted"
+@app.route("/admin-only", methods=["GET"])
+@jwt_required()
+def admin_only():
+    claims = get_jwt()
+    if claims.get("role") != "admin":
+        return jsonify({"error": "Admin access required"}), 403
+    return "Admin Access: Granted"
+if __name__ == "__main__":
+    app.run()
