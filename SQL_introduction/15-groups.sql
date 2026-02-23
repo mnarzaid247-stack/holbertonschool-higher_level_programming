a73c6ca558cfade9f17d487f6ccb3401398b17f0
manar <mnarzaid247@gmail.com>
2026-02-23 09:51:32 +0000

    update

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list a number
-SELECT COUNT(*) AS number, score FROM second_table GROUP BY score ORDER BY number DESC; 
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC; 
