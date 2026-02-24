79c709cd997d4e61fca133254f0c7527d6379fac
manar <mnarzaid247@gmail.com>
2026-02-24 11:17:04 +0000

    update

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- showing data
-SELECT id, name FROM cities INNER JOIN states 
-ON states.id = cities.id
-ORDER BY id ASC
+SELECT cities.id,cities.name, states.name FROM cities INNER JOIN states 
+ON cities.state_id = states.id
+ORDER BY cities.id ASC
