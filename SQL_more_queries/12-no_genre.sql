62fc1d85ea25165a72840ad9eba4a8a7b0e45284
manar <mnarzaid247@gmail.com>
2026-02-25 08:51:21 +0000

    update

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- showing data
 SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres
+RIGHT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC , tv_show_genres.genre_id ASC;
