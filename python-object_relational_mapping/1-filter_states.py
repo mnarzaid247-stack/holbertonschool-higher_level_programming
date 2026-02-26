56621dc4935797bf699331deeace1e39c8e05aef
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-02-26 11:50:17 +0300

    Fix indentation for database connection parameters

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,12 +13,12 @@ def main():
     password = sys.argv[2]
     database = sys.argv[3]
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-            )
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     for row in cur.fetchall():
