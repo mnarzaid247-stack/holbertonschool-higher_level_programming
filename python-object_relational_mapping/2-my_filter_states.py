f012051137b8bb16ef38d9c22358f4ba2ad8e805
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-02-26 13:10:21 +0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,8 @@ def main():
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(sys.argv[4])
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;"
+        .format(sys.argv[4])
     )
     for row in cur.fetchall():
         print(row)
