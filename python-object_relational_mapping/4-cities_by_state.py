76a05eed71e6c9c7f4fd3563edd6bbb3653f68d3
manar <mnarzaid247@gmail.com>
2026-02-26 11:35:54 +0000

    doing task4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists all ceties"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """main functhion"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+
+
+if __name__ = "__main__":
+    main()
