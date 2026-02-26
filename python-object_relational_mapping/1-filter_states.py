dcec57817f757ae876351d51a3de30771c1cac04
manar <mnarzaid247@gmail.com>
2026-02-26 08:46:00 +0000

    doing task1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""lists with condithons"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """the main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '%N' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
