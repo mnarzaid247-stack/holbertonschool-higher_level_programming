8c6b5f4bc5001ccc4af5c15c111f1ba74c50919b
manar <mnarzaid247@gmail.com>
2026-02-27 15:52:47 +0000

    doing task10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""lists with name"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(username, password, database),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == name).order_by(State.id).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
