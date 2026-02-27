bf674580151e2739df1a2842137efcdd678b2090
manar <mnarzaid247@gmail.com>
2026-02-27 04:56:08 +0000

    doing task9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,4 +4,23 @@
 import sys
 from model_state import State, Base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(username, password, database),
+            pool_pre_ping=True
+            )
+    Session = sessionmaker(bind=engine)
+    session=Session()
+    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
+    session.close()
+
+if __name__ == "__main__":
+    main()
