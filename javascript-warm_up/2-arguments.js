0107dd019e0d756fbb34798db18dc9a22e1d8b10
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-13 18:54:16 +0300

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.length - 2
+const args = process.argv.length - 2;
 
 if (args === 0) {
-  console.log('No argument')
+  console.log('No argument');
 } else if (args === 1) {
-  console.log('Argument found')
+  console.log('Argument found');
 } else {
-  console.log('Arguments found')
+  console.log('Arguments found');
 }
