c06a7771508412818b39f96b8d7b6b3a9f540cf6
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-29 15:40:43 +0300

    Create 1-script.js

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('#red_header');
+const header = document.querySelector('header');
+
+btn.addEventListener('click', function () {
+  header.style.color = '#FF0000';
+});
