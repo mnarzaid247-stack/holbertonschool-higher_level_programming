05a829c68a66ae705a0aad6f9f68f7994b71a5c3
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-29 15:47:23 +0300

    Create 3-script.js

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('#toggle_header');
+const header = document.querySelector('header');
+
+btn.addEventListener('click', function () {
+  if (header.classList.contains('red')) {
+    header.classList.remove('red');
+    header.classList.add('green');
+  } else {
+    header.classList.remove('green');
+    header.classList.add('red');
+  }
+});
